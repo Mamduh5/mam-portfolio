cbf7c3c72c1e7a35577c8f4aed854809d146c42e
Mamduh <mamduh2542@gmail.com>
2026-03-17 18:15:45 +0700

    add countdown logic when post visit

--- src/hooks/useVisit.js
+++ src/hooks/useVisit.js
@@ -7,7 +7,21 @@ export const useVisit = () => {
   const location = useLocation()
 
   useEffect(() => {
+
+    const key = "last_visit_log"
+    const now = Date.now()
+
+    const last = localStorage.getItem(key)
+
+    // 10 seconds cooldown
+    if (last && now - last < 10000) {
+      return
+    }
+
+    localStorage.setItem(key, now)
+
     logVisit(location.pathname)
+
   }, [location])
 
 }
