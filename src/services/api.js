8ea3697bf4369ac5e7d7bfab7ae8ad3ae4b9035d
Mamduh <86569881+Mamduh5@users.noreply.github.com>
2026-05-30 02:41:40 +0700

    Default API client to same origin proxy

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,8 @@
 import axios from "axios"
 
+const proxyPath = ["", "api"].join("/")
+const baseURL = import.meta.env.VITE_API_URL || proxyPath
+
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL
 })
