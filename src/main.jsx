a1cf95887cf2798817728ef4980196b3a3c5d239
Mamduh <86569881+Mamduh5@users.noreply.github.com>
2026-05-29 17:51:38 +0700

    Load admin dashboard styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 import App from "./App.jsx"
 import "./index.css"
 import "./landing-extra.css"
+import "./admin.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
