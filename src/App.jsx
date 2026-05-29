375cc4039b5d5d4005890e654a554611002e50ff
Mamduh <86569881+Mamduh5@users.noreply.github.com>
2026-05-29 17:50:10 +0700

    Add protected admin routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,45 @@ import Home from "./pages/Home"
 import Games from "./pages/Games"
 import Contact from "./pages/Contact"
 import Login from "./pages/Login"
+import AdminOverview from "./pages/admin/AdminOverview"
+import AdminMessages from "./pages/admin/AdminMessages"
+import AdminProjects from "./pages/admin/AdminProjects"
+import AdminProfile from "./pages/admin/AdminProfile"
+import AdminUploads from "./pages/admin/AdminUploads"
 import ConsoleShell from "./components/ConsoleShell"
+import AdminShell from "./components/AdminShell"
+import ProtectedRoute from "./components/ProtectedRoute"
 import { useVisit } from "./hooks/useVisit"
+
+function PublicShell({ children }) {
+  return <ConsoleShell>{children}</ConsoleShell>
+}
 
 function App() {
   useVisit()
 
   return (
-    <ConsoleShell>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/games" element={<Games />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </ConsoleShell>
+    <Routes>
+      <Route path="/" element={<PublicShell><Home /></PublicShell>} />
+      <Route path="/projects" element={<PublicShell><Projects /></PublicShell>} />
+      <Route path="/games" element={<PublicShell><Games /></PublicShell>} />
+      <Route path="/contact" element={<PublicShell><Contact /></PublicShell>} />
+      <Route path="/login" element={<PublicShell><Login /></PublicShell>} />
+      <Route
+        path="/admin"
+        element={(
+          <ProtectedRoute>
+            <AdminShell />
+          </ProtectedRoute>
+        )}
+      >
+        <Route index element={<AdminOverview />} />
+        <Route path="messages" element={<AdminMessages />} />
+        <Route path="projects" element={<AdminProjects />} />
+        <Route path="profile" element={<AdminProfile />} />
+        <Route path="uploads" element={<AdminUploads />} />
+      </Route>
+    </Routes>
   )
 }
 
